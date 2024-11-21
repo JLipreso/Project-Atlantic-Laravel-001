17e8830923f80244e23fffe52ae2570fd0c1b01a
JLipreso <lipreso.developer@gmail.com>
2024-11-22 00:13:25 +0800

    OK

--- app/Http/Controllers/db_warehouse/TBLStocksFetch.php
+++ app/Http/Controllers/db_warehouse/TBLStocksFetch.php
@@ -117,7 +117,7 @@ class TBLStocksFetch extends Controller
 
             return [
                 "success"       => true,
-                "header"        => $stock,
+                "header"        => \App\Http\Controllers\db_warehouse\ObjectParser::tbl_stocks($stock),
                 "locators"      => \App\Http\Controllers\db_warehouse\TBLStocksLocator::fetchLocators($stock['item_no']),
                 "photos"        => [],
                 "permissions"   => [
