965aed9a5df4a0b290464ff409377ee69699847d
JLipreso <lipreso.developer@gmail.com>
2024-12-06 05:31:44 +0800

    OK

--- app/Http/Controllers/db_warehouse/TBLWSDetailsFetch.php
+++ app/Http/Controllers/db_warehouse/TBLWSDetailsFetch.php
@@ -54,7 +54,50 @@ class TBLWSDetailsFetch extends Controller
     }
 
     public static function inputReleaseDone(Request $request) {
-        $items = DB::connection('db_warehouse')->table('tbl_wsdetail')->where("ctrl_no", $request['ctrl_no'])->get();
-        return $items;
+        $items  = DB::connection('db_warehouse')->table('tbl_wsdetail')->where("ctrl_no", $request['ctrl_no'])->get();
+        $sum    = DB::connection('db_warehouse')
+                    ->table('tbl_stock_locator_history')
+                    ->where([
+                        ['ws_ctrl_no', $request['ws_ctrl_no']],
+                        ['ws_detail_ctrl_no', $request['ws_detail_ctrl_no']],
+                    ])
+                    ->sum('quantity');
+        if(count($items) == 0) {
+            return [
+                "success"   => false,
+                "message"   => "No record found"
+            ];
+        }
+        else {
+            foreach($items as $item) {
+                if(floatval($item['rel_unit']) < 0) {
+                    return [
+                        "success"   => false,
+                        "message"   => "Error: Input quantity cannot be less than zero. Please enter a valid value."
+                    ];
+                    break;
+                }
+                else if(floatval($item['rel_unit']) > floatval($item['qty_unit'])) {
+                    return [
+                        "success"   => false,
+                        "message"   => "Error: Input quantity exceeds locator quantity. Please verify and adjust the values."
+                    ];
+                    break;
+                }
+                else if(($sum > floatval($item['qty_unit'])) && ($request['mode'] !== '5-RESTOCK')) {
+                    return [
+                        "success"   => false,
+                        "message"   => "Error: Release quantity cannot exceed the requested quantity"
+                    ];
+                    break;
+                }
+                else {
+                    return [
+                        "success"   => true,
+                        "message"   => "Done"
+                    ];
+                }
+            }
+        }
     }
 }
